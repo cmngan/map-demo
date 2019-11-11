985c8634fc03bb0627d0fb55a8b3cbaf265fc783
CM Ngan <plusonecm@gmail.com>
2019-11-11 13:48:35 +0800

    update default apiEndpoint

--- src/config.js
+++ src/config.js
@@ -7,5 +7,5 @@ const urlSearchParams =  window.location.search.slice(1).split('&').reduce( (p, 
   }, {})
 
 export const MAP_API_KEY = urlSearchParams.mapApiKey || window.localStorage.getItem('mapApiKey') || '' // should be moved to BE
-export const BACKEND_API_ENDPOINT = urlSearchParams.apiEndpoint || window.localStorage.getItem('apiEndpoint') || 'https://please-set-api-endpoint-by-setting-localStorage-with-key-apiEndpoint'
+export const BACKEND_API_ENDPOINT = urlSearchParams.apiEndpoint || window.localStorage.getItem('apiEndpoint') || ''
 export const CSS_MOBILE_BREAKPOINT = 700
